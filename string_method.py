1e30d0890de3103e1dfa9a4335c85431dcf845c3
singh7970 <priyanshu2032004@gmail.com>
2024-02-03 00:55:38 +0530

    stringh_method

---
+++ string_method.py
@@ -0,0 +1,6 @@
+# a=" abcdefghijk lmnop  qrstuvwxy"
+# print(a.strip()) # The strip() method removes any whitespace from the beginning or the end:
+
+a="ab cde ghi klmn o pqrs tuv w xy"
+# print(a.replace("a","qkuf" ))
+print(a.split())
