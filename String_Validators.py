39c826486db14758553951a2b24ea6d4d159cf4b
singh7970 <priyanshu2032004@gmail.com>
2024-02-03 01:31:48 +0530

    Feb 3

---
+++ String_Validators.py
@@ -0,0 +1,8 @@
+a="qA2"
+print(a.isalnum())
+print(any(i.isalnum() for i in a) )
+# print(a.islower())
+# print(a.isspace())
+# print(a.istitle())
+# print(a.isupper())
+# print(a.lower())
--- string_method.py
+++ string_method.py
@@ -1,6 +1,10 @@
 # a=" abcdefghijk lmnop  qrstuvwxy"
 # print(a.strip()) # The strip() method removes any whitespace from the beginning or the end:
 
-a="ab cde ghi klmn o pqrs tuv w xy"
+a=" abcd efghijk lmn op  qrstuv xy"
+
 # print(a.replace("a","qkuf" ))
-print(a.split())
+# print(a.split())
+# x = a.zfill(10)#zeros until it is 10 characters long
+# print(a.title())#Converts the first character of each word to upper case
+print(a.rjust(60))
