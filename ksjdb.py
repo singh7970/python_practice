4dadb3f9052ac5e745bf3f4fa72df601bc4c672b
singh7970 <priyanshu2032004@gmail.com>
2024-02-03 02:39:45 +0530

    jh

--- ksjdb.py
+++ ksjdb.py
@@ -1,3 +1,11 @@
-# print("hello my name s ")lkjxbxcjzbcjx
-# hvczxkhcvkhcvzkjckajsbkjb
-# akakjbajb
+import sys
+
+# sample Sets
+Set1 = {"A", 1, "B", 2, "C", 3}
+Set2 = {"Geek1", "Raju", "Geek2", "Nikhil", "Geek3", "Deepanshu"}
+Set3 = {(1, "Lion"), ( 2, "Tiger"), (3, "Fox")}
+
+# print the sizes of sample Sets
+print("Size of Set1: " + str(sys.getsizeof(Set1)) + "bytes")
+print("Size of Set2: " + str(sys.getsizeof(Set2)) + "bytes")
+print("Size of Set3: " + str(sys.getsizeof(Set3)) + "bytes")
