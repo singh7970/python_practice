9520360dd472480d1cc6787372f290f537c3854d
Priyanshu Singh <priy2032004anshu@gmail.com>
2024-02-04 01:06:47 +0530

    Aligment

--- String_Text_Alignment.py
+++ String_Text_Alignment.py
@@ -1,2 +1,16 @@
-width = 20
-print ('HackerRank'.center(width,'-'))
+# width = 50
+# print ('H'.ljust(width,'-'))
+# print ('H'.center(width,'-'))
+# print ('H'.rjust(width,'-'))
+thickness = int(input()) #This must be an odd number
+c = 'H'
+for i in range(thickness):
+    print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+for i in range(thickness+1):  print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+
+for i in range((thickness+1)//2):
+    print((c*thickness*5).center(thickness*6))    
+
+for i in range(thickness+1): print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))    
+
+for i in range(thickness):  print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))    
