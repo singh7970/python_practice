2bdd7dcb54f6e3184058675d55ac9aff06fc506e
Priyanshu Singh <priy2032004anshu@gmail.com>
2024-02-14 23:59:00 +0530

    feb 14 update

--- number_Conversion.py
+++ number_Conversion.py
@@ -1,4 +1,8 @@
-n=int(input("Enter the number"))
-print(bin(n)[2:],"binary")
-print(oct(n)[2:],"octal")
-print(hex(n)[2:],"hexadecimal")
+n=int(input("Enter the number"))#user input number 
+print(" 1 bin")
+print(" 2 oct")
+print(" 3 hex")
+
+print(bin(n)[2:],"binary")#Convert to binary form
+print(oct(n)[2:],"octal")#Convert to ocatl form
+print(hex(n)[2:],"hexadecimal")#Convert to hexadecimal  form
