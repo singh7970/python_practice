f9b470dd6c198b3ef2306738f3dce9fb183e536d
Priyanshu Singh <priy2032004anshu@gmail.com>
2024-02-17 10:00:07 +0530

    feb 17

--- 2_Randomnum.py
+++ 2_Randomnum.py
@@ -1,3 +1,15 @@
+# import random
+# x = random.randint(1,10)
+# print("Randint",x)
+# print(random.randrange(1,4))
+# l=[12,123,112,3443,2343,4,46,6,54,]
+# print(random.choice(l))
+
+# print(random.random())
+# p=[12,123,112,3443,2343,4,46,6,54]
+# random.shuffle(p)
+# print(p)
+# print("1020012")
 import random 
 print("Enter the number between 1-10")
 comp=random.randint(1,10)
