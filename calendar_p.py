8d1273dad3edfca1f3a399212483ff64550cb611
Priyanshu Singh <priy2032004anshu@gmail.com>
2024-02-19 00:48:10 +0530

    feb 19

---
+++ calendar_p.py
@@ -0,0 +1,13 @@
+# import calendar
+
+
+# year=int(input("Enter the year"))
+# month=int(input("Enter the month"))
+# cal=calendar.month(year,month)
+# print(cal)
+
+
+import calendar
+year = 2024
+print(dir(calendar))
+calendar.prcal(year)
