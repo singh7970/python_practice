302a0c518e709c11a60dad1137393d5fe89c50d5
Priyanshu Singh <priy2032004anshu@gmail.com>
2024-02-28 23:53:02 +0530

    temp

--- AddTwoMatrices.py
+++ AddTwoMatrices.py
@@ -1,42 +1,28 @@
-# A=[[1,2,3],
-#    [4,5,6],
-#    [7,8,9]]
-# B=[[3,2,4],
-#    [5,6,8],
-#    [5,3,6]]
-# re=[[0,0,0],
-#     [0,0,0],
-#     [0,0,0]]
-# for i in range(len(A)):
-#     for j in range(len(B)):
-#         re[i][j]=A[i][j]+B[i][j]
-# for r in re:
-#     print(r)
+# row=int(input("Enter the row number"))
+# col=int(input("Enter the column number"))
+# matrix=[]
+# for i in range(row):
+#     tem=[]
+#     for j in range(col):
+#         tem.append(int(input))
+# matrix.append(tem)	
 
-
-
-#take matrix input from user
-Row = int(input("Enter the number of rows:"))
-Column = int(input("Enter the number of columns:"))
-
-# Initialize matrix
+# for i in range(row):
+#     for j in range(col):
+#         print(matrix[i][j],end='')
+# print( )	
+row = int(input("Enter the number of rows: "))
+col = int(input("Enter the number of columns: "))
 matrix = []
-print("Enter the entries row wise:")
 
-# For user input
-# A for loop for row entries
-for row in range(Row): 
-	a = []
-	# A for loop for column entries
-	for column in range(Column): 
-		a.append(int(input()))
-		print(a)
-	matrix.append(a)
-
-
+for i in range(row):
+    temp = []
+    for j in range(col):
+        temp.append(int(input()))
+    matrix.append(temp)
 
 # For printing the matrix
-for row in range(Row):
-	for column in range(Column):
-		print(matrix[row][column], end=" ")
-	print()
+for i in range(row):
+    for j in range(col):
+        print(matrix[i][j], end=' ')
+    print()
