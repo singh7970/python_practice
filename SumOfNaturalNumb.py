d3be257ff46a2be7977a91730bfb96a1699097fb
Priyanshu Singh <priy2032004anshu@gmail.com>
2024-03-01 13:20:29 +0530

    update

--- SumOfNaturalNumb.py
+++ SumOfNaturalNumb.py
@@ -1,9 +1,19 @@
-l=[]
-n=int(input("Enter the number :"))
-for i in range(1,n+1):
+# l=[]
+# n=int(input("Enter the number :"))
+# for i in range(1,n+1):
 
-    l.append(i)    
-print(l)    
-print(sum(l))
+#     l.append(i)    
+# print(l)    
+# print(sum(l))
 
 # print(sum(s))
+a=1
+b=2
+c=2
+l=[]
+for i in range(3):
+    l.append(a+i)
+
+s=set(l)
+print(sum(s))
+print(type(s))
