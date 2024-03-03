2768de673d66d75a3e56c5b237a6a0de3cd82ec7
Priyanshu Singh <priy2032004anshu@gmail.com>
2024-03-03 22:26:32 +0530

    joinfunc

--- reverse.py
+++ reverse.py
@@ -1,8 +1,26 @@
-a=int(input("enter the NUMber"))
+# a=input("enter the NUMber")
+# print(a[::-1])
 
-c=0
-for i in range (1,len(str(a))+1):
-    b=a%10
-    c=c*10+b
-    a=a//10
-print(c)
+
+#2
+# a=int(input("enter the NUMber"))
+# c=0
+# for i in range (1,len(str(a))+1):
+#     b=a%10
+#     c=c*10+b
+#     a=a//10
+# print(c)
+
+
+# 3
+# s="sky is blue"
+# print(s[::-1])
+
+
+s="sky is blue"
+r=" ".join(reversed(s.split()))
+print(r)
+
+# l = ['hello', 'my name is']
+# l.reverse()
+# print(l)
