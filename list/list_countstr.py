61d6a8a6259641255d1339bef51e263150c370de
Priyanshu Singh <priy2032004anshu@gmail.com>
2024-03-20 18:46:10 +0530

    countstr

--- list/list_countstr.py
+++ list/list_countstr.py
@@ -3,3 +3,17 @@
 
  Sample List : ['abc', 'xyz', 'aba', '1221']
  Expected Result : 2'''
+
+
+l= ['abc', 'zx','abaa', '1221','1211','3213']
+
+count=0
+for i in l:
+        if  len(i)>=4 and i[0] == i[-1]:
+        # if  i[0] =='a':
+            
+            count += 1
+print(count)            
+# c=l[2]
+# print(c)
+# print(c[1])
