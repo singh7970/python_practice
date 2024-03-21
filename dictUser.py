f0c703d627a017a463f199eb6cfd0134783408db
Priyanshu Singh <priy2032004anshu@gmail.com>
2024-03-21 18:10:30 +0530

    dict user

---
+++ dictUser.py
@@ -0,0 +1,6 @@
+user={'name':'Alice','age':20}
+chek= 'age'
+if chek in user:
+    print("Found")
+else:
+    print("not")    
--- list/list_min_max.py
+++ list/list_min_max.py
@@ -1,6 +1,9 @@
-l = [1, 2, 3, 4, 54, 355, 57, 3, 56, 6, 35, 45, 6, 7]
-l.sort()
-print("second min",l[1])
-print("second max",l[-2])
+# l = [1, 2, 3, 4, 54, 355, 57, 3, 56, 6, 35, 45, 6, 7]
+# l.sort()
+# print("second min",l[1])
+# print("second max",l[-2])
 
-
+#compare
+l1=[1,2,'a','b']
+l2=[1,2,'a','b']
+print(l1==l2 ,l1 is  l2)
