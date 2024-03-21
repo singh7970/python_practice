c6c4539505d7e680ff17887c39c8d7f56f01210e
Priyanshu Singh <priy2032004anshu@gmail.com>
2024-03-22 00:10:17 +0530

    up

---
+++ list/listshuffle.py
@@ -0,0 +1,5 @@
+# Write a Python program to shuffle and print a specified list
+import random
+l = [1, 2, 3, 4, 54, 355, 57, 3, 56, 6, 35, 45, 6, 7]
+random.shuffle(l)
+print(l)
--- numpy/0darray.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-
-arr = np.array([1, 2, 3, 4],[1,3,2,2,])
-
-print(arr[0])
