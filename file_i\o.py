e9fb7bf0fcabb340b16c1efedecd8ad7f45467c3
Priyanshu Singh <priy2032004anshu@gmail.com>
2024-04-02 03:43:55 +0530

    file

---
+++ file_i\o.py
@@ -0,0 +1,3 @@
+filef = open('hello.txt', 'x')  # Open the file in 'x' mode to create it
+filef.write("Hello, world!")    # Write content into the file
+filef.close()                    # Close the file
