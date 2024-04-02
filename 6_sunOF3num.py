dbbf4be913c374ee4a6dfa2508c3cd631279fd12
Priyanshu Singh <priy2032004anshu@gmail.com>
2024-04-03 00:56:26 +0530

    sumof3

---
+++ 6_sunOF3num.py
@@ -0,0 +1,10 @@
+'''Write a program that will give you the sum of 3 digits
+'''
+print("Enter the  3 digits")
+a=int(input("Enter the number "))
+b=int(input("Enter the number "))
+c=int(input("Enter the number "))
+
+t=a+b+c
+print(f'total of {a},{b},{c} is {t}')
+
